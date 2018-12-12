a97597e63673830be51780edb5c154894a778b14
dongmeimei <dongmeimei@job-mate.com>
2018-12-12 19:59:03 +0800

    gengxin

--- frontend/frontend-vue/src/cart/ProductsList.vue
+++ frontend/frontend-vue/src/cart/ProductsList.vue
@@ -19,7 +19,7 @@
         },
         methods: {
             ...mapActions('cart',[
-              'addProductToCart'  
+                'addProductToCart'  
             ])
         },
         created(){
